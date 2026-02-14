c2b5696758c27f9ae277b25366d1e253c2f3ea60
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-02-14 17:28:34 +0300

    8.2 Применяем Pydantic в HTTP API клиентах

--- api_client_get_documents.py
+++ api_client_get_documents.py
@@ -12,18 +12,18 @@ print("Create user response:", create_user_response, "\n")
 
 # Открываем кредитный счет
 open_credit_card_account_response = accounts_gateway_client.open_credit_card_account(
-    create_user_response["user"]["id"]
+    create_user_response.user.id
 )
 print("Open credit card account response:", open_credit_card_account_response, "\n")
 
 # Получаем документ тарифа
 get_tariff_document_response = documents_gateway_client.get_tariff_document(
-    open_credit_card_account_response["account"]["id"]
+    open_credit_card_account_response.account.id
 )
 print("Get tariff document response:", get_tariff_document_response, "\n")
 
 # Получение документа контракта
 get_contract_document_response = documents_gateway_client.get_contract_document(
-    open_credit_card_account_response["account"]["id"]
+    open_credit_card_account_response.account.id
 )
 print("Get contract document response:", get_contract_document_response, "\n")
