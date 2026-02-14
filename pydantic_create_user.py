c2a2e9b3bd627708017e1af2993540f86a8c396c
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-02-14 12:06:30 +0300

    8.1 Знакомство с Pydantic. Практика.

---
+++ pydantic_create_user.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserSchema(BaseModel):
+    """Модель данных пользователя."""
+
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """Запрос на создание пользователя."""
+
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserResponseSchema(BaseModel):
+    """Ответ с данными созданного пользователя."""
+
+    user: UserSchema
