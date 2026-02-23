a308adfaea63456771f3f8019441842fbd2820e2
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-02-23 14:58:28 +0300

    10.1 Знакомство с Locust. ПРавктика.

---
+++ locust_open_debit_card_account.py
@@ -0,0 +1,39 @@
+from locust import HttpUser, between, task
+from tools.fakers import fake
+
+
+class OpenDebitCardAccountScenarioUser(HttpUser):
+    # Пауза между запросами для каждого виртуального пользователя (в секундах)
+    wait_time = between(1, 3)
+
+    # В этой переменной будем хранить данные созданного пользователя
+    user_data: dict
+
+    def on_start(self) -> None:
+        """
+        Метод on_start вызывается один раз при запуске каждой сессии виртуального пользователя.
+        Здесь мы создаем нового пользователя, отправляя POST-запрос к /api/v1/users.
+        """
+        request = {
+            "email": fake.email(),
+            "lastName": fake.last_name(),
+            "firstName": fake.first_name(),
+            "middleName": fake.middle_name(),
+            "phoneNumber": fake.phone_number()
+        }
+        response = self.client.post(url="/api/v1/users", json=request)
+
+        # Сохраняем полученные данные, включая ID пользователя
+        self.user_data = response.json()
+
+    @task
+    def open_debit_card(self):
+        """
+        Основная нагрузочная задача: открытие дебетового счета..
+        Здесь мы выполняем POST-запрос к /api/v1/accounts/open-debit-card-account.
+        """
+        request = {"userId": self.user_data["user"]["id"]}
+        self.client.post(
+            url="/api/v1/accounts/open-debit-card-account",
+            json=request
+        )
