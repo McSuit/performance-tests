6ae97f47abd77c899326a98867e265bbc57632cf
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-03-09 15:29:25 +0300

    10.6 Сценарии нагрузки в Locust: User, TaskSet и SequentialTaskSet. Практика.

---
+++ grpc_locust_get_accounts.py
@@ -0,0 +1,59 @@
+from locust import User, between, task
+from clients.grpc.gateway.locust import GatewayGRPCTaskSet
+from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserResponse
+from contracts.services.gateway.accounts.rpc_open_deposit_account_pb2 import OpenDepositAccountResponse
+
+
+class GetAccountsTaskSet(GatewayGRPCTaskSet):
+    """
+    Нагрузочный сценарий, который последовательно:
+    1. Создает нового пользователя
+    2. Открывает депозитный счёт для этого пользователя
+    3. Получает список всех счетов, связанных с пользователем
+
+    Использует базовый GatewayHTTPTaskSet и уже созданных в нём API клиентов.
+    """
+
+    create_user_response: CreateUserResponse | None = None
+    open_deposit_account_response: OpenDepositAccountResponse | None = None
+
+    @task(2)
+    def create_user(self):
+        """
+        Создаём нового пользователя и сохраняем результат для последующих шагов.
+        """
+        self.create_user_response = self.users_gateway_client.create_user()
+
+    @task(2)
+    def open_deposit_account(self):
+        """
+        Открываем депозитарный счёт для созданного пользователя.
+        Проверяем, что предыдущий шаг был успешным.
+        """
+        if not self.create_user_response:
+            return
+
+        self.open_deposit_account_response = self.accounts_gateway_client.open_deposit_account(
+            user_id=self.create_user_response.user.id
+        )
+
+    @task(6)
+    def get_accounts(self):
+        """
+        Запрашиваем список всех счетов для текущего пользователя..
+        """
+        if not self.create_user_response:
+            return
+
+        self.accounts_gateway_client.get_accounts(
+            user_id=self.create_user_response.user.id
+        )
+
+
+class GetDocumentsScenarioUser(User):
+    """
+    Пользователь Locust, исполняющий непоследовательный сценарий получения счетов.
+    """
+    host = "localhost"
+    tasks = [GetAccountsTaskSet]
+    wait_time = between(1, 3)
